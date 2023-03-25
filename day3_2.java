0a511bc17ac06c4611f50118d82e65be91750f4e
Pruthvi00 <115860339+Pruthvi00@users.noreply.github.com>
2023-03-26 00:04:40 +0530

    completed the part 2 of day 3

---
+++ day3_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class day3_2 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of sides of rectangle :");
+        int length = sc.nextInt();
+        int breadth = sc.nextInt();
+        int Area_of_rectangle = length * breadth ; 
+        System.out.println("The area of rectangle is : " + Area_of_rectangle);
+    }
+}
